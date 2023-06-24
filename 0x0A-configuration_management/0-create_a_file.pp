d6dacb00499f883c9062df7aa2b62e6d6e362a2c
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-24 12:32:31 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Using Puppet, create a file in /tmp.
 
 file { '/tmp/school':
-  ensure  => file
-  mode    => '0744'
-  group   => 'www-data'
-  owner   => 'www-data'
+  ensure  => file,
+  mode    => '0744',
+  group   => 'www-data',
+  owner   => 'www-data',
   content => "I love Puppet",
 }
