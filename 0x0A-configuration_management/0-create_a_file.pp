417cf3bdd6c63e5fa4169bcbe722a18126d69b36
Bayode oluwatosin <113604922+Tosinvee@users.noreply.github.com>
2023-06-29 19:15:18 -0700

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Using Puppet, create a file in /tmp.
-
-file { '/tmp/school':
-  ensure  => present,
-  mode    => '0744',
-  group   => 'www-data',
-  owner   => 'www-data',
-  content => 'I love Puppet',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-#Using Puppet, create a manifest that kills a process named killmenow.
-exec { 'killmenow':
-    command => 'pkill -f "killmenow"',
-    path    => '/usr/local/bin/:/bin/',
-  }
